7aeec201cc8cf9ad4d2e237c7c433d53af9d05b8
Manekineko-mk1 <1919892+Manekineko-mk1@users.noreply.github.com>
2021-03-23 19:17:43 -0400

    Configure Caliburn Micro and SimpleContainer
    Modify Bootstrapper.cs to fit our MVVM framework. Includes the SimpleContainer DI system.

--- JRMDesktopUI/Bootstrapper.cs
+++ JRMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,61 @@ namespace JRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        // for dependency injection
+        // This will handle the instantiation all the classes
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            // Whenever someone asks for a SimpleContainer instance, we will return the _container instance.
+            _container.Instance(_container);
+
+            // WindowManager : Bringing windows in and out
+            // EventAggregator : We can pass event messages throughout our application, kinda like eventlistener
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Use reflection on the current instance
+            // From every types, filter to IsClass AND name = "ViewModel"
+            // Return the result to a list
+            // For each element in the result (viewModelType), use _container and call SimpleContainer.RegisterPerRequest()
+            // RegisterPerRequest() => Register the class , so that a new instance is created for every request
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
+        // When the application starts, it will call the ShellViewModel
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        // When we pass in a type and a name (key)
+        // Caliburn will get us that instance
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        // This is uses to construct things
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
